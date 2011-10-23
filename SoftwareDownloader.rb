f5a2e2a26d15b52de8fe88d6230d47e30a9fcb29
Uri Gorelik <uri.gore@gmail.com>
2011-10-22 23:11:29 -0400

    Fixed for windows

--- SoftwareDownloader.rb
+++ SoftwareDownloader.rb
@@ -13,7 +13,7 @@ class SoftwareDownloader
 	def set_os
 		os = RUBY_PLATFORM.downcase
 	  @browser_command = "open"
-	  @browser_command = "start" if os.start_with?('mswin') || os[/mingw32/] != nil
+	  @browser_command = "explorer" if os.start_with?('mswin') || os[/mingw32/] != nil
 	end
 	
 	def download
@@ -24,8 +24,8 @@ class SoftwareDownloader
 			url = @url.gsub(/#{@replace_str}/, "#{s}+download")
 			puts "#{@url}\nThis is the url #{url}"
 			
-			system "#{@browser_command} " + "\'#{url}\'"
-#			gets
+			command = "#{@browser_command} " + "\"#{url}\""
+			system command 
 		end
 		 
 	end
